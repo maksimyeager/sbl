74ab84ffe1372c28860f554056bf45d53fded8c6
maksimyeager <maksimyeager@gmail.com>
2024-08-07 20:39:23 +0400

    Resolve problems with marshruts and some style changes

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ i18next.init({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter basename={"/"}>
             <I18nextProvider i18n={i18next}>
                 <App />
             </I18nextProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/sbl',
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.htaccess',
+          dest: ''
+        }
+      ]
+    })
+  ],
   server: {
     port: 5173
   },
